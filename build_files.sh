d5d975b832d6803711f6a3bf47b18f0cdc7c757e
fawad <fawadstar6@gmail.com>
2022-11-26 20:59:34 +0500

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo " BUILD START"
-python3.9  -m venv venv
-python3.9  venv\scripts\activate
+python  -m venv venv
+python  venv\scripts\activate
 python3.9  -m pip install -r requirements.txt
 python3.9 manage.py collectstatic  --noinput --clear
 echo " BUILD END"
