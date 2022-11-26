1ae5b08df3fd2f97fe805162678bf891a3326351
fawad <fawadstar6@gmail.com>
2022-11-26 15:38:35 +0500

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python 3.10.2  -m pip install -r requirements.txt
-python 3.10.2 manage.py collectstatic  --noinput --clear
+python -m pip install -r requirements.txt
+python manage.py collectstatic  --noinput --clear
 echo " BUILD END"
